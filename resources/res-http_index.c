2a872b157e4c70f635c1b6f5850e91ce24c12e95
Prates <cpr4t3s@gmail.com>
2017-03-26 14:37:54 +0100

    .

--- resources/res-http_index.c
+++ resources/res-http_index.c
@@ -4,12 +4,12 @@
 #include "rest-engine.h"
 
 static void res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
-
+static void res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 
 RESOURCE(res_http_index,
 		  "title=\"HTTP Index\";obs",
 		  res_get_handler,
-		  NULL,
+		  res_post_handler,
 		  NULL,
 		  NULL);
 
@@ -25,6 +25,13 @@ res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferr
 
 	REST.set_response_payload(response, message, length);
 	REST.set_header_content_type(response, REST.type.TEXT_PLAIN); /* text/plain is the default, hence this option could be omitted. */
+}
+
+static void
+res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	REST.set_header_content_type(response, REST.type.APPLICATION_JSON);
+	REST.set_response_status(response, REST.status.OK);
 }
 
 
